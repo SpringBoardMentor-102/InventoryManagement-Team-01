a21c330f63b02962ac15c6e05eadc2d8e8383f13
Nancy Dutta <129151532+NancyDutta@users.noreply.github.com>
2024-04-21 22:48:22 +0530

    Add files via upload

---
+++ 01_UX/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import SignIn from './components/SignIn'
+import Reset from './components/Reset'
+import SignUp from './components/SignUp';
+import Home from './Home';
+import ForgotPassword from './components/ForgotPassword';
+
+import NotFound from './components/NotFound';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+const App = () => {
+  return (
+
+<BrowserRouter>
+  <Routes>
+    <Route index element={<SignIn/>}/>
+    <Route path='/signin' element={<SignIn/>}/>
+    <Route path='/signup' element={<SignUp/>}/>
+    <Route path='/reset' element={<Reset/>}/>
+    <Route path='/home'  element={<Home/>}/>
+    <Route path='/forgot'  element={<ForgotPassword/>}/>
+    <Route path='*' element={<NotFound/>}/>
+  </Routes>
+</BrowserRouter>
+  )
+}
+
+export default App
---
+++ 01_UX/src/components/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>welcome to signup page</div>
+  )
+}
+
+export default SignUp
