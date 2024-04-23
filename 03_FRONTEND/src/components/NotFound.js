0e31ec5fa1aae813a3e462e2037033e769cb83c3
Nancy Dutta <129151532+NancyDutta@users.noreply.github.com>
2024-04-23 17:10:44 +0530

    Update NotFound.js

--- 03_FRONTEND/src/components/NotFound.js
+++ 03_FRONTEND/src/components/NotFound.js
@@ -8,7 +8,7 @@ const NotFound = () => {
     <div style={{textAlign: 'center'}}>
         <h1 className="error-heading_404">404 - Page Not Found</h1>
         <p className="error-message_404">Oops! The page you are looking for could not be found.</p>
-        <Link  to="/home" className="button_404">Go to Home Page</Link>
+        <Link  to="/Dashboard" className="button_404">Go to Home Page</Link>
     </div>
     </div>
   )
