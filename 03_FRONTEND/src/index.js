714eef9e0bc53e175769b8f3cde673f252e2b47b
Nancy Dutta <129151532+NancyDutta@users.noreply.github.com>
2024-04-23 16:47:27 +0530

    Update index.js

--- 03_FRONTEND/src/index.js
+++ 03_FRONTEND/src/index.js
@@ -1,18 +1,6 @@
 import React from'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './index.css'
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-
-// ReactDOM.render(<h1><App/></h1>,document.getElementById("root"));
+ReactDOM.render(<h1><App/></h1>,document.getElementById("root"));
