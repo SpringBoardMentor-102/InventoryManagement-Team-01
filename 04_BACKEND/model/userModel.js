1d643404369b366ea73c14eb742d33076e1e55e2
Aaditya Dhondkar <98811579+AadityaDhondkar@users.noreply.github.com>
2024-04-23 17:31:05 +0530

    Updated User Schema
    Updated user schema to store reset password token and expiry date

--- 04_BACKEND/model/userModel.js
+++ 04_BACKEND/model/userModel.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid');
+const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
-    userId: { type: String, default: uuidv4 },
-    email: { type: String, unique: true, required: true },
+const userSchema = new Schema({
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     phone: { type: String, required: true },
     roles: { type: Number, required: true },
-    city: { type: String, required: true }
+    city: { type: String, required: true },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date }
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
