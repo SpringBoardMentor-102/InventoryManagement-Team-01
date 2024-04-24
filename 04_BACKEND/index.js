bb7cdb4635ba259d0f12c0cf00a1a8d8dbe81194
prayagpc <prayagpc28@gmail.com>
2024-04-24 21:23:31 +0530

    Added cors

--- 04_BACKEND/index.js
+++ 04_BACKEND/index.js
@@ -1,24 +1,26 @@
 const express = require("express");
+var cors = require("cors");
 const bodyParser = require("body-parser");
 const config = require("dotenv/config");
-require('dotenv').config({ path: './04_BACKEND/.env' });
+require("dotenv").config({ path: "./04_BACKEND/.env" });
 const mongoose = require("mongoose");
 
 const categories = require("./routes/categoriesRoutes.js");
 const checkouts = require("./routes/checkoutRoutes.js");
 const connectDB = require("./server.js");
 const users = require("./routes/userRoutes.js");
-const transactions = require('./routes/transactionRoutes.js'); 
+const transactions = require("./routes/transactionRoutes.js");
 const products = require("./routes/productRoutes.js");
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/users", users);
 app.use("/api/checkout", checkouts);
 app.use("/api/category", categories);
-app.use('/api/transaction', transactions);
+app.use("/api/transaction", transactions);
 app.use("/api/product", products);
 
 const PORT = process.env.PORT || 5000;
