5183fc03b7a4e029f2f57130c581b1ba5af5ada1
Nancy Dutta <129151532+NancyDutta@users.noreply.github.com>
2024-05-04 11:53:36 +0530

    done api call for product list

--- 03_FRONTEND/src/components/ProductList.js
+++ 03_FRONTEND/src/components/ProductList.js
@@ -1,23 +1,47 @@
-import React from 'react'
-import  ProductData from './ProductData.json'
+import axios from 'axios';
+import React,{useState,useEffect} from 'react';
 const ProductList = () => {
+ const [products,setProducts]=useState([]);
+ const[loading,setLoading]=useState(true);
+ const[error,setError]=useState(null);
+
+ useEffect(()=>{
+  const apiUrl='http://localhost:5000/api/product/getAllProducts';
+  const fetchData=async()=>{
+    try{
+      const response=await axios.get(apiUrl);
+      setProducts(response.data.products);
+      setLoading(false);
+    }catch(error){
+   setError(error.message);
+   setLoading(false);
+    }
+  };
+  fetchData();
+ }, []);
+ if(loading){
+  return <div> loading...</div>;
+ }
+ if(error){
+  return <div>Error :{error}</div>;
+ }
  
 
   return (
 
 <div className='product-container'>
-{ProductData.map(item=>(
+{products.map(item=>(
     <div className="item-container">
-      <img className="item-image" src={item.image} alt="item image" />
-      <div className="rating">
+      <img className="item-image" src={item.imageUrl} alt="item image" />
+      {/* <div className="rating">
         {item.rating.stars} ⭐ | {item.rating.count}
-      </div>
-      <div className="company-name">{item.company}</div>
-      <div className="item-name">{item.item_name}</div>
+      </div> */}
+      <div className="company-name">{item.name}</div>
+      <div className="item-name">{item.description}</div>
       <div className="price">
-        <span className="current-price">Rs {item.current_price}</span>
-        <span className="original-price">Rs {item.original_price}</span>
-        <span className="discount">({item.discount_percentage}% OFF)</span>
+        <span className="current-price">Rs {item.price}</span>
+        {/* <span className="original-price">Rs {item.original_price}</span>
+        <span className="discount">({item.discount_percentage}% OFF)</span> */}
       </div>
       </div>
 ))}
