b6ed2df79183176e8d8654598a00d916759d8914
SpringBoardMentor-102 <166400737+SpringBoardMentor-102@users.noreply.github.com>
2024-05-06 16:17:18 +0530

    Rearranging App.js for organization.

--- 03_FRONTEND/src/App.js
+++ 03_FRONTEND/src/App.js
@@ -1,4 +1,8 @@
+// External Imports
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Internal Component Imports
 import SignIn from './components/SignIn'
 import Reset from './components/Reset'
 import Confirm from './components/Confirm'
@@ -7,29 +11,30 @@ import Dashboard from './components/Dashboard';
 import ForgotPassword from './components/ForgotPassword';
 import EmailNotification from './components/EmailNotification';
 import NotFound from './components/NotFound';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Reg_Config from './components/Reg_Config';
 import Search from './components/Search';
 import Filter from './components/Filter';
-const App = () => {
-  return (
 
-<BrowserRouter>
-  <Routes>
-    <Route index element={<SignIn/>}/>
-    <Route path='/signin' element={<SignIn/>}/>
-    <Route path='/signup' element={<SignUp/>}/>
-    <Route path='/reg_config' element={<Reg_Config/>}/>
-    <Route path='/reset' element={<Reset/>}/>
-    <Route path="/confirm-email" element={<Confirm />} />
-    <Route path='/Dashboard'  element={<Dashboard/>}/>
-    <Route path='/forgot'  element={<ForgotPassword/>}/>
-    <Route path='/email_notification'  element={<EmailNotification/>}/>
-    <Route path='/search_product'  element={<Search/>}/>
-    <Route path='*' element={<NotFound/>}/>
-    <Route path='/Filter' element ={<Filter/>}/>
-  </Routes>
-</BrowserRouter>
+// create an app to render routes
+const App = () => {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<SignIn />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/reg_config' element={<Reg_Config />} />
+        <Route path='/reset' element={<Reset />} />
+        <Route path="/confirm-email" element={<Confirm />} />
+        <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path='/forgot' element={<ForgotPassword />} />
+        <Route path='/email_notification' element={<EmailNotification />} />
+        <Route path='/search_product' element={<Search />} />
+        <Route path='/Filter' element={<Filter />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
