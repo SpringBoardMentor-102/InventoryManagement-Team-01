b804c0fd8e4bbe7f9d1525399c6ed0fd98fbcc28
SpringBoardMentor-102 <166400737+SpringBoardMentor-102@users.noreply.github.com>
2024-05-06 16:31:20 +0530

    Removed unused imports from Dashboard.js

--- 03_FRONTEND/src/components/Dashboard.js
+++ 03_FRONTEND/src/components/Dashboard.js
@@ -1,18 +1,13 @@
-import React, { useState } from 'react'
-
 import ProductList from './ProductList';
 import Sidebar from './Sidebar';
 import Search from './Search';
 
 const Dashboard = () => {
-
-
   return (
     <div className="dash-container">
-
       <Sidebar />
-
       <main>
+        <Search />
         <div>
           <ProductList />
         </div>
