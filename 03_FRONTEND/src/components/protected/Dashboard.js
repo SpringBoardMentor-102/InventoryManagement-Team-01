e05eab5c1ad56e4b49869356006c428f6c66433d
SpringBoardMentor-102 <166400737+SpringBoardMentor-102@users.noreply.github.com>
2024-05-09 14:24:34 +0530

    Removed unnecessary code from Dashboard.js

--- 03_FRONTEND/src/components/protected/Dashboard.js
+++ 03_FRONTEND/src/components/protected/Dashboard.js
@@ -1,32 +1,13 @@
-import ProductList from "./ProductList";
 import Sidebar from "./Sidebar";
 import Search from "./Search";
-import React, { useEffect, useState } from "react";
-import { fetchData } from "../../utilities/validators/apputils";
 
 const Dashboard = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const fetchProducts = async () => {
-    try {
-      const response = await fetchData("product/getAllProducts");
-      setProducts(response);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching products:", error);
-    }
-  };
-  useEffect(() => {
-    fetchProducts();
-  }, []);
 
   return (
     <div className="dash-container">
       <Sidebar />
       <main>
         <Search />
-        <div>{/* <ProductList /> */}</div>
       </main>
     </div>
   );
