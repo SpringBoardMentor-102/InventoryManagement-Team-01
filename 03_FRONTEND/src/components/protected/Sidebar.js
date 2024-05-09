20dcff7d99f0a49e094b4ed49c02a2a26aa8c503
SpringBoardMentor-102 <166400737+SpringBoardMentor-102@users.noreply.github.com>
2024-05-09 15:13:42 +0530

    Removed warnings and added signout functionality to Sidebar.js

--- 03_FRONTEND/src/components/protected/Sidebar.js
+++ 03_FRONTEND/src/components/protected/Sidebar.js
@@ -1,65 +1,72 @@
-import React ,{useState} from 'react'
-import img1 from "../../images/logo.png"
-import SearchBar from '../../components/protected/Searchbar';
+import React, { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+import img1 from "../../images/logo.png"
 const Sidebar = () => {
-    const [isOpen,setIsOpen]=useState(false);
-    
-    const toggleSidebar=()=>{
+
+    const navigate = useNavigate()
+
+    // get the current location
+    const location = useLocation();
+    const activeLink = location.pathname;
+    console.log("This is the activeLink: ", activeLink)
+
+    const [isOpen, setIsOpen] = useState(true);
+    const toggleSidebar = () => {
         setIsOpen(!isOpen);
     }
-  return (
 
-<aside>
+    const handleLogout = () => {
+        sessionStorage.removeItem("token")
+        navigate("/signin")
+    }
+
+    return (
+        <aside>
             <div className="top">
                 <div className="logo">
-                    <img src={img1} alt=""/>
+                    <img src={img1} alt="" />
                     <h2>INFO <span className="danger">SYS</span></h2>
                 </div>
-               
+
                 <div className="close" id="close-btn">
-                    <span className="material-icons-sharp">close</span>                
-                    </div>
+                    <span className="material-icons-sharp">close</span>
+                </div>
             </div>
 
             {/* note : a link shouldbe converted to LINK  */}
-         <button onClick={toggleSidebar}><FontAwesomeIcon icon={faBars}/></button>
-         {isOpen && 
-         
-            <div className="sidebar">
-                <a href="#">
-                    <span className="material-icons-sharp">grid_view</span>
-                    <h3>Dashboard</h3>
-                </a>
-                <a href="#" className="active">
-                    <span className="material-icons-sharp">inventory</span>
-                    <h3>Inventory</h3>
-                </a>
-            
-                <a href="#">
-                    <span className="material-icons-sharp">reorder</span>
-                    <h3>Orders</h3>
-                </a>
-                <a href="#">
-                    <span className="material-icons-sharp">add_shopping_cart</span>
-                    <h3>Cart</h3>
-                </a>
-                <a href="#">
-                    <span className="material-icons-sharp">manage_accounts</span>
-                    <h3>Help Center</h3>
-                </a>
-                
-                <a href="#">
-                    <span className="material-icons-sharp">logout</span>
-                    <h3>Logout</h3>
-                </a>
-            </div>
-         }
+            <button onClick={toggleSidebar}><FontAwesomeIcon icon={faBars} /></button>
+            {isOpen &&
+
+                <div className="sidebar">
+                    <a href="/" className={activeLink === '/dashboard' ? "active" : ""}>
+                        <span className="material-icons-sharp">grid_view</span>
+                        <h3>Dashboard</h3>
+                    </a>
+                    <a href="/" className={activeLink.startsWith("/product/") ? "active" : ""}>
+                        <span className="material-icons-sharp">details</span>
+                        <h3>Product</h3>
+                    </a>
+                    <a href="/history" className={activeLink === '/history' ? "active" : ""}>
+                        <span className="material-icons-sharp">history</span>
+                        <h3>History</h3>
+                    </a>
+                    <a href="/help">
+                        <span className="material-icons-sharp">manage_accounts</span>
+                        <h3>Help Center</h3>
+                    </a>
+                    <a href="/" onClick={ handleLogout }>
+                        <span className="material-icons-sharp">logout</span>
+                        <h3>Logout</h3>
+                    </a>
+                </div>
+            }
 
         </aside>
 
-  )
+    )
 }
 
 export default Sidebar
