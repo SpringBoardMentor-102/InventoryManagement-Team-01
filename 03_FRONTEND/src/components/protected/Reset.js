f4ab800d7b4d70314dbee70cb1ddc339a19929d2
ItzPriyanshuTech <priyanshuchakraborty543@gmail.com>
2024-05-18 17:00:33 +0530

    removed fetchData

--- 03_FRONTEND/src/components/protected/Reset.js
+++ 03_FRONTEND/src/components/protected/Reset.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { fetchData, fetchDataUnprotected } from "../../utilities/apputils";
+import { fetchDataUnprotected } from "../../utilities/apputils";
 
 
 //internal dependencies
