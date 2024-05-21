73b3feb9445674c121eb5ba19242ac19d8035438
VaibhavTawle2273 <167400778+Vaibhav01tawale@users.noreply.github.com>
2024-05-21 23:55:33 +0530

    Added SideBar And CSS to Create New Product  Page

--- 03_FRONTEND/src/components/protected/ManageProduct.js
+++ 03_FRONTEND/src/components/protected/ManageProduct.js
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from "react";
 import { fetchData } from "../../utilities/apputils";
 import { useNavigate } from "react-router-dom";
+import AdminSidebar from "../protected/AdminSidebar";
+import "../../index.css";
 //internal dependecies
 import {
   validateCategoryId,
@@ -173,158 +175,134 @@ const ManageProduct = () => {
     }
   };
 
-  //   //     const formData = new FormData();
-  //   //     formData.append('name', data.name);
-  //   //     formData.append('description', data.description);
-  //   //     formData.append('price', data.price);
-  //   //     formData.append('quantity', data.quantity);
-  //   //     formData.append('status', data.status);
-  //   //     formData.append('category_id', data.categoryId);
-  //   //     formData.append('imageUrl', data.imageUrl);
-  //   // console.log(formData,  "formadata");
-  //   // console.log("---------------",data, typeof data);
+  useEffect(() => {
+  
+  })
 
-  //   try {
-  //     const response = await fetchData("post", `product/createProduct`, {
-  //       name: data.name,
-  //       description: data.description,
-  //       price: data.price,
-  //       quantity: data.quantity,
-  //       status: data.status,
-  //       categoryId: data.categoryId,
-  //       imageUrl: data.imageUrl,
-  //     });
-  //     console.log("on new product form", response);
-  //   } catch (error) {
-  //     console.error("There was an error adding the product!", error);
-  //   }
-  // };
-
-  // const handleImageChange = (e) => {
-  //   setProductImage(e.target.files[0]);
-  // };
 
   return (
-    <>
-      {/* <AdminSidebar /> */}
-      <div className="manage-page">
-        <div className="manage-container">
-          <h2 className="manage-heading">MANAGE PRODUCT</h2>
-          <div id="errorMessage" className="error_sign_up">
-            {errorMessage}
+    <div className="dash-container">
+    <div >
+      <AdminSidebar />
+    </div>
+      <div>
+        <div className="Add_product">
+          <div className="new_product">
+            <h2>Add New Product</h2>
+            <div id="errorMessage" className="error_sign_up">
+              {errorMessage}
+            </div>
+            <form onSubmit={createProduct}>
+            <div  className="div_of_input_element">
+                <label className="label_price">Name:</label>
+                <input className="product_name"
+                  type="text"
+                  value={productName}
+                  placeholder=" Product Name"
+                  onChange={(e) => setProductName(e.target.value)}
+                  required
+                />
+              </div>
+              <div id="ProductNameError" className="error_sign_up">
+                {ProductNameError}
+              </div>
+              <div  className="div_of_input_element" >
+                <label className="label_price">Description:</label>
+                <textarea  className="product_desc"
+                  value={productDescription}
+                  placeholder=" Product Description"
+                  onChange={(e) => setProductDescription(e.target.value)}
+                  required
+                ></textarea>
+              </div>
+              <div id="productDescriptionError" className="error_sign_up">
+                {productDescriptionError}
+              </div>
+              <div className="price_quantity" >
+                <div  >
+                  <label className="label_price_quantity">Price:</label>
+                  <input className="product_price"
+                    type="number"
+                    min="0"
+                    value={productPrice}
+                    placeholder=" Product Price"
+                    onChange={(e) => setProductPrice(e.target.value)}
+                    required
+                  />
+                </div>
+                <div id="productPriceError" className="error_sign_up">
+                  {productPriceError}
+                </div>
+                <div >
+                  <label className="label_price_quantity">Quantity:</label>
+                  <input
+                    className="product_price"
+                    type="number"
+                    min="0"
+                    
+                    value={productQuantity}
+                    placeholder=" Product Quantity"
+                    onChange={(e) => setProductQuantity(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div id="productQuantityError" className="error_sign_up">
+                {productQuantityError}
+              </div>
+              <div  className="div_of_input_element">
+                <label className="label_price" >Status:</label>
+                <select
+                  className="product_name"
+                  onChange={(e) => setProdcutStatus(e.target.value)}
+                >
+                  <option value="available">Available</option>
+                  <option value="out_of_stock">Out of Stock</option>
+                </select>
+              </div>
+              <div id="prodcutStatusError" className="error_sign_up">
+                {prodcutStatusError}
+              </div>
+              <div  className="div_of_input_element">
+                <label className="label_price">Category ID:</label>
+                <select
+                  className="product_name"
+                  value={productCategory}
+                  onChange={(e) => setProductCategory(e.target.value)}
+                  required
+                >
+                  <option value="">Select Category</option>
+                  {categories.map((category) => (
+                    <option key={category._id} value={category._id}>
+                      {category.categoryName}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div id="productCategoryError" className="error_sign_up">
+                {productCategoryError}
+              </div>
+              <div  className="div_of_input_element">
+                <label className="label_price">Product Image:</label>
+                <input
+                
+                  type="text"
+                  value={productImage}
+                  placeholder=" Product Image URL"
+                  onChange={(e) => setProductImage(e.target.value)}
+                  required
+                />
+              </div>
+              <div id="productImageError" className="error_sign_up">
+                {productImageError}
+              </div>
+              <button type="submit">Add Product</button>
+            </form>
           </div>
-          <form className="form-container" onSubmit={createProduct}>
-            <div className="manage-name">
-              <label>Name:</label>
-              <input
-               className="manage-product"
-                type="text"
-                value={productName}
-                placeholder="Product Name"
-                onChange={(e) => setProductName(e.target.value)}
-                required
-              />
-            </div>
-            <div id="ProductNameError" className="error_sign_up">
-              {ProductNameError}
-            </div>
-            <div>
-              <label>Description:</label>
-              <textarea
-               className="manage-product"
-                value={productDescription}
-                placeholder="Product Description"
-                onChange={(e) => setProductDescription(e.target.value)}
-                required
-              ></textarea>
-            </div>
-            <div id="productDescriptionError" className="error_sign_up">
-              {productDescriptionError}
-            </div>
-            <div>
-              <div>
-                <label>Price:</label>
-                <input
-                 className="manage-product"
-                  type="number"
-                  min="0"
-                  value={productPrice}
-                  placeholder="Product Price"
-                  onChange={(e) => setProductPrice(e.target.value)}
-                  required
-                />
-              </div>
-              <div id="productPriceError" className="error_sign_up">
-                {productPriceError}
-              </div>
-              <div>
-                <label>Quantity:</label>
-                <input
-                 className="manage-product"
-                  type="number"
-                  min="0"
-                  // {...register("quantity")}
-                  value={productQuantity}
-                  placeholder="Product Quantity"
-                  onChange={(e) => setProductQuantity(e.target.value)}
-                  required
-                />
-              </div>
-            </div>
-            <div id="productQuantityError" className="error_sign_up">
-              {productQuantityError}
-            </div>
-            <div>
-              <label>Status:</label>
-              <select
-              className="manage-product"
-                onChange={(e) => setProdcutStatus(e.target.value)}
-              >
-                <option value="available"  className="manage-product">Available</option>
-                <option value="out_of_stock"  className="manage-product">Out of Stock</option>
-              </select>
-            </div>
-            <div id="prodcutStatusError" className="error_sign_up">
-              {prodcutStatusError}
-            </div>
-            <div>
-              <label>Category ID:</label>
-              <select 
-               className="manage-product"
-                value={productCategory}
-                onChange={(e) => setProductCategory(e.target.value)}
-                required
-              >
-                <option value="">Select Category</option>
-                {categories.map((category) => (
-                  <option key={category._id} value={category._id}>
-                    {category.categoryName}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div id="productCategoryError" className="error_sign_up">
-              {productCategoryError}
-            </div>
-            <div className="manage-name">
-              <label>Product Image:</label>
-              <input
-               className="manage-product"
-                type="text"
-                value={productImage}
-                placeholder="Product Image URL"
-                onChange={(e) => setProductImage(e.target.value)}
-                required
-              />
-            </div>
-            <div id="productImageError" className="error_sign_up">
-              {productImageError}
-            </div>
-            <button type="submit">Add Product</button>
-          </form>
         </div>
       </div>
-    </>
+    
+    </div>
   );
 };
 
