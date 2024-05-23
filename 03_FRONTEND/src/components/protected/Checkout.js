7850d4b87c1005407073a96d95ca394b28b22337
Amanletscode <aman123mot@gmail.com>
2024-05-24 04:34:33 +0530

    Improved CSS of OrderSummary page and implemented few features like confirm before checkout and place order just before actual transaction starts.

--- 03_FRONTEND/src/components/protected/Checkout.js
+++ 03_FRONTEND/src/components/protected/Checkout.js
@@ -3,12 +3,14 @@ import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { fetchData } from "../../utilities/apputils";
+import ConfirmationModal from "./Confirmbeforecheckout";
 
 const Checkout = () => {
   const [items, setItems] = useState([]);
   const [totalAmount, setTotalAmount] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const navigate = useNavigate(); 
-  
+
   useEffect(() => {
     const fetchCartItems = () => {
       const cart = JSON.parse(localStorage.getItem('cart')) || [];
@@ -22,39 +24,40 @@ const Checkout = () => {
     const total = cartItems.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
     setTotalAmount(total);
   };
-  
+
   const handleCheckout =async () => {
     if (items.length === 0) {
       alert("Your Cart is empty. Please add products!!! ");
       return;
     }
+    setIsModalOpen(true);
+  };
+
+  const confirmCheckout = async () => {
     const userId = JSON.parse(localStorage.getItem("user")).id;
+    const cart = JSON.stringify(items);
 
-    console.log(userId)
-    const cart= localStorage.getItem("cart")
-    console.log(cart);
-    const confirmation = window.confirm("Are you sure you want to checkout?");
-    if (confirmation) {
-      // Navigate to the order summary page with the selected products
-      try {
-        await fetchData("post", "checkout/addcheckout",{
-          user_id: userId,
-          cart: cart 
-        })
-        navigate('/order-summary', { state: { selectedProducts: items } });
-      } catch (error) {
-        console.log(error);
-      }
+    try {
+      await fetchData("post", "checkout/addcheckout",{
+        user_id: userId,
+        cart: cart 
+      });
+      navigate('/order-summary', { state: { selectedProducts: items } });
+    } catch (error) {
+      console.log(error);
+    }
 
-    // Clear cart data after successful checkout (or handle errors gracefully)
     try {
       localStorage.removeItem("cart");
-      setItems([]); // Update state for immediate UI reflection
+      setItems([]); 
     } catch (error) {
       console.error("Error clearing cart:", error);
-      // Implement error handling (e.g., display an error message)
     }
-  }
+    setIsModalOpen(false);
+  };
+
+  const cancelCheckout = () => {
+    setIsModalOpen(false);
   };
 
   const handleImageClick = (productId) => {
@@ -64,9 +67,8 @@ const Checkout = () => {
   return (
     <>
       <Link to="/Dashboard" className="back-to-dashboard-btn">
-      <FontAwesomeIcon icon={faArrowLeft} /> Back to Dashboard
+        <FontAwesomeIcon icon={faArrowLeft} /> Back to Dashboard
       </Link>
-
       <div className="checkout-container">
         <div className="items-container">
           <div className="checkout-heading">
@@ -87,7 +89,7 @@ const Checkout = () => {
               <div key={item.product._id} className="check-name">
                 <img className="img-checkout" src={item.product.imageUrl} alt={item.product.name} />
                 <div className="checkout-name">{item.product.name}</div>
-                <div className="checkout-price">${item.product.price}</div>
+                <div className="checkout-price">₹{item.product.price.toFixed(2)}</div>
                 <div className="checkout-quantity">{item.quantity}</div>
               </div>
             ))
@@ -97,6 +99,11 @@ const Checkout = () => {
           </button>
         </div>
       </div>
+      <ConfirmationModal 
+        isOpen={isModalOpen} 
+        onConfirm={confirmCheckout} 
+        onCancel={cancelCheckout} 
+      />
     </>
   );
 };
