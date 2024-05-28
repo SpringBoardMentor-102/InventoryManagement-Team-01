9dc79c22b70c6750eddf75fc09a10a883eed5ed7
ItzPriyanshuTech <priyanshuchakraborty543@gmail.com>
2024-05-28 21:08:04 +0530

    Updated Footer.js

--- 03_FRONTEND/src/components/protected/Footer.js
+++ 03_FRONTEND/src/components/protected/Footer.js
@@ -3,35 +3,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faFacebook, faTwitter,faYoutube,faInstagram} from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom'
 
+// Footer component
 const Footer = () => {
   return (
     <footer>
-<div className="footer">
-<div className="row">
-<Link to="#"><FontAwesomeIcon icon={faFacebook} size='2x'/></Link>
- <Link to="#"><FontAwesomeIcon icon={faInstagram} size='2x'/></Link>
-<Link to="#"><FontAwesomeIcon icon={faTwitter} size='2x'/></Link>
-<Link to="#"><FontAwesomeIcon icon={faYoutube} size='2x'/></Link> 
-
-</div>
-
-<div className="row">
-<ul>
-<li><Link to='#' >Contact us</Link></li>
-<li><Link to='#' >Our Services</Link></li>
-<li><Link to='#' >Privacy Policy</Link></li>
-<li><Link to='#' >Terms & Conditions</Link></li>
-<li><Link to='#' >Career</Link></li>
-
-</ul>
-</div>
-
-<div className="row">
-INFO SYS Copyright © 2021 INFO SYS - All rights reserved || Designed By: InventoryMangement Team
-</div>
-</div>
-</footer>
+      <div className="footer">
+        <div className="row"> {/*Social media icons*/}
+          <Link to="#"><FontAwesomeIcon icon={faFacebook} size='2x'/></Link>
+          <Link to="#"><FontAwesomeIcon icon={faInstagram} size='2x'/></Link>
+          <Link to="#"><FontAwesomeIcon icon={faTwitter} size='2x'/></Link>
+          <Link to="#"><FontAwesomeIcon icon={faYoutube} size='2x'/></Link> 
+        </div>
+        
+        <div className="row">
+          <ul>
+            <li><Link to='#' >Contact us</Link></li>
+            <li><Link to='#' >Our Services</Link></li>
+            <li><Link to='#' >Privacy Policy</Link></li>
+            <li><Link to='#' >Terms & Conditions</Link></li>
+            <li><Link to='#' >Career</Link></li>
+          </ul>
+        </div>
+        
+        {/*Copyright Information*/}
+        <div className="row">
+          INFO SYS Copyright © 2021 INFO SYS - All rights reserved || Designed By: InventoryMangement Team
+        </div>
+      </div>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer;
