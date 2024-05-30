6acbd4926760b14379cd3531a35d97b8608688d5
Om Natekar <natekarom1@gmail.com>
2024-05-30 18:02:33 +0530

    code cleanup searchbar.js

--- 03_FRONTEND/src/components/protected/Searchbar.js
+++ 03_FRONTEND/src/components/protected/Searchbar.js
@@ -1,17 +1,14 @@
-import React  from 'react'
+import React from 'react'
 
 const SearchBar = () => {
-
   return (
-
     <div className="search-bar">
-      <input type="text" id="search-input" placeholder="Search here..."/> 
-        <button type="button" id="search-btn">
-          <span className="material-icons-sharp">search</span>
-        </button>
-        
+      <input type="text" id="search-input" placeholder="Search here..." />
+      <button type="button" id="search-btn">
+        <span className="material-icons-sharp">search</span>
+      </button>
     </div>
   )
 }
 
-export default SearchBar;
+export default SearchBar
