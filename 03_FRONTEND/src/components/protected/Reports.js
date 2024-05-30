1e9bfed898afd639a55cf8fcf46b87db72fa2867
Rupakumari5860 <134612200+Rupakumari5860@users.noreply.github.com>
2024-05-30 08:49:16 +0530

    Redirection of item in report to product detail

--- 03_FRONTEND/src/components/protected/Reports.js
+++ 03_FRONTEND/src/components/protected/Reports.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { fetchData } from "../../utilities/apputils";
 import Sidebar from "./Sidebar";
+import { useNavigate } from 'react-router-dom';
+
 
 // Define the Reports component
 const Reports = () => {
 
   // Initialize the state variable 'products' as an empty array using useState
   const [products, setProducts] = useState([]);
-
+  const navigate = useNavigate();
+ 
   useEffect(() => {
 
     // Define an asynchronous function to fetch products data
@@ -28,6 +31,14 @@ const Reports = () => {
     // Call the fetchProducts function when the component mounts
     fetchProducts();
   }, []);
+  
+
+
+   // Navigate to the product detail page
+   const handleItemClick = (productId) => {
+    navigate(`/product/${productId}`);
+  }
+  
 
   // Return the JSX to render the component
   return (
@@ -51,7 +62,7 @@ const Reports = () => {
             {/*Map over 'products' array to generate table rows*/}
             {products.map((item, index) => (
               <tr key={index}>
-                <td>{item.name}</td>
+                <td onClick={() => handleItemClick(item._id)} style={{ cursor: 'pointer' }}>{item.name}</td>
                 <td>
                   {item.quantity > 0 ? '✔️' : '❌'} {/* Table cell for availability, showing a checkmark if quantity > 0, otherwise a cross */}
                 </td>
