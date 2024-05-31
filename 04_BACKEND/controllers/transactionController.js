d2a669300c859aafab8c434b48cdba66642e1b14
Devanshi_kadecha <141355756+Devanshi-Soni@users.noreply.github.com>
2024-06-01 00:10:34 +0530

    Added comments in transactionController.js

--- 04_BACKEND/controllers/transactionController.js
+++ 04_BACKEND/controllers/transactionController.js
@@ -1,12 +1,14 @@
 const Transaction = require("../model/transactionModel");
 const mongoose = require("mongoose");
 
+// Function to create a new transaction
 async function createTransaction(req, res) {
   try {
     const newTransaction = new Transaction(req.body);
     // Validate the new transaction against the schema
     const validationResult = newTransaction.validateSync();
     if (validationResult) {
+      // Extract and format validation errors
       const errors = Object.values(validationResult.errors).map(
         (error) => error.message
       );
@@ -14,8 +16,9 @@ async function createTransaction(req, res) {
         .status(400)
         .json({ message: "Invalid transaction data", errors });
     }
-
+    // Save the new transaction to the database
     await newTransaction.save();
+    // Return success response with the created transaction
     res.status(201).json({
       message: "Transaction created successfully",
       transaction: newTransaction,
@@ -26,24 +29,29 @@ async function createTransaction(req, res) {
   }
 }
 
+// Function to get all transactions based on optional filters
 async function handleGetAlltransaction(req, res) {
   const filters = {};
   const { userId, checkoutId, startDate, endDate } = req.query;
+
+  // Apply filters based on query parameters
   if (userId) filters.userId = userId;
   if (checkoutId) filters.checkoutId = checkoutId;
   if (startDate && endDate) filters.date = { $gte: startDate, $lte: endDate };
   else if (startDate) filters.date = { $gte: startDate };
   else if (endDate) filters.date = { $lte: endDate };
 
+  // Find transactions based on filters
   Transaction.find(filters)
     .then((transactions) => res.status(200).json({ transactions }))
     .catch((err) => res.status(500).json({ message: "Server error" }));
 }
 
+// Function to get a transaction by user ID
 async function handlegetTransactionByID(req, res) {
   try {
     //  const transaction = await Transaction.findById(req.params.id)
-    // Now transaction will be fecthed on the basis of userId
+    // Find transaction(s) by user ID and populate related fields
     const transaction = await Transaction.find({ userId: req.params.id })
       .populate("userId")
       .populate("checkoutId")
@@ -55,27 +63,26 @@ async function handlegetTransactionByID(req, res) {
 
     res.status(200).json({ transaction });
   } catch (err) {
+    // Log and return server error
     console.error(err.message);
     res.status(500).json({ message: "Server error", error: err.message });
   }
 }
 
+// Function to update a transaction
 async function handleUpdatesTransactions(req, res) {
   try {
-
-    // if (!req.user || req.user.role !== 'admin') {
-    //   return res.status(403).json({ message: "Insufficient permissions" });
-    // }
-
+    // Check if the provided ID is a valid ObjectId
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       return res.status(400).json({ message: "Invalid order ID" });
     }
 
-    // Convert itemId string to ObjectId
+   // Convert itemId strings to ObjectIds in the request body
     req.body.items = req.body.items.map((item) => ({
       ...item,
       itemId: new mongoose.Types.ObjectId(item.itemId),
     }));
+    // Find and update the transaction by ID
     const updatedTransaction = await Transaction.findByIdAndUpdate(
       req.params.id,
       req.body,
@@ -85,10 +92,11 @@ async function handleUpdatesTransactions(req, res) {
       }
     );
 
+    // Check if the transaction was found and updated
     if (!updatedTransaction) {
       return res.status(404).json({ message: "Transaction not found" });
     }
-
+    // Return the updated transaction
     res.status(200).json({ transaction: updatedTransaction });
   } catch (err) {
     console.error(err.message);
@@ -97,25 +105,23 @@ async function handleUpdatesTransactions(req, res) {
       .json({ message: "Invalid transaction data", error: err.message });
   }
 }
+
+// Function to delete a transaction
 async function handleDeleteTransaction(req, res) {
   try {
-
-    // if (!req.user || req.user.role !== 'admin') {
-    //   return res.status(403).json({ message: "Insufficient permissions" });
-    // }
-
+    // Check if the provided ID is a valid ObjectId
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       return res.status(400).json({ message: "Invalid order ID" });
     }
-
+    // Find and delete the transaction by ID
     const deletedTransaction = await Transaction.findByIdAndDelete(
       req.params.id
     );
-
+    // Check if the transaction was found and deleted
     if (!deletedTransaction) {
       return res.status(404).json({ message: "Transaction not found" });
     }
-
+    // Return success message
     res.status(200).json({ message: "Transaction deleted successfully" });
   } catch (err) {
     console.error(err.message);
