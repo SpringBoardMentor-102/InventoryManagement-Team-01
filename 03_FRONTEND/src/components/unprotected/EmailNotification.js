42ad2d8ad5ed717b0cef22261b99c869927a58bd
Devanshi_kadecha <141355756+Devanshi-Soni@users.noreply.github.com>
2024-05-31 23:05:14 +0530

    Update EmailNotification.js

--- 03_FRONTEND/src/components/unprotected/EmailNotification.js
+++ 03_FRONTEND/src/components/unprotected/EmailNotification.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// EmailNotification component to remind the user to check their email
 const EmailNotification = () => {
   return (
     <div className="email-container">
+      {/* Heading to indicate the user hasn't checked their email */}
       <h2>Ooops! It seems like you haven't checked your Email</h2>
       <p>To access your account, please take a moment to check your Email.</p>
       <p>If you didn't receive an email, please go through the following points:</p>
+
+      {/* List of steps to ensure the user receives the email */}
       <ul className="flex-vertically">
         <li>
           <b>Complete Form:</b> Ensure all required fields are filled out accurately.
@@ -22,6 +26,8 @@ const EmailNotification = () => {
         </li>
       </ul>
       <p>Thank you for choosing us. We look forward to having you fully onboarded!</p>
+
+      {/* Button to navigate the user to the SignUp page */}
       <Link to="/SignUp">
         <button type="submit" className="button-email-notification">
           Complete Your Registration
